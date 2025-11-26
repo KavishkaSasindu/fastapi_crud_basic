900c66ce271b9e08a421ee3fcf04eb309fa489ed
sasindu0215@gmail.com <sasindu0215@gmail.com>
2025-11-27 02:02:19 +0530

    updated

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -1,6 +1,12 @@
 from app.db.base import Base
-from app.db.session import asycn_engine
+from app.db.session import asycn_engine,AsyncSessionLocal
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import AsyncSession
 
 async def init_db():
     async with asycn_engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+        
+async def get_session() -> AsyncGenerator[AsyncSession,None]:
+    async with AsyncSessionLocal() as session:
+        yield session
